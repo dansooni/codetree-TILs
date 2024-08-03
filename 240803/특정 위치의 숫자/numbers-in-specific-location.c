008c5dd766523d6af183973f03b1dccb05c41dc0
Song Jonggeun <49198110+dansooni@users.noreply.github.com>
2024-08-03 10:59:12 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/특정 위치의 숫자/numbers-in-specific-location.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int sum = 0;
+
+    for(int i=0; i<10; i++) {
+        scanf("%d", arr+i);
+    }
+
+    printf("%d", arr[2]+arr[4]+arr[9]);
+    
+    return 0;
+}
