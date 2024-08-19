2fd43dfde38911c09462e303d9e1c18b514fa0df
Song Jonggeun <49198110+dansooni@users.noreply.github.com>
2024-08-19 14:17:58 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/1-9 개수 세기/count-one-to-nine.c
+++ 240819/1-9 개수 세기/count-one-to-nine.c
@@ -4,11 +4,11 @@ int main() {
     int arr[10] = {0, };
     int n;
     scanf("%d", &n);
-    int input[n];
+    int input;
 
     for(int i=0; i<n; i++) {
-        scanf("%d", input+i);
-        arr[input[i]]++;
+        scanf("%d", &input);
+        arr[input]++;
     }
     
     for(int i=1; i<10; i++) {
