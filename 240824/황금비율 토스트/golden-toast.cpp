30ce423bac1b2b1c5dcc00199a010c3ed35a3287
Song Jonggeun <49198110+dansooni@users.noreply.github.com>
2024-08-24 14:49:38 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240824/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    string alphabets;
+    char command, new_alphabet;
+    list<char> breads;
+    list<char>::iterator it;
+
+    cin >> n >> m;
+    cin >> alphabets;
+
+    for(int i=0; i<alphabets.size(); i++) {
+        breads.push_back(alphabets[i]);
+    }
+
+    it = breads.end();
+    for(int i=0; i<m; i++) {
+        cin >> command;
+
+        if(command=='L') {
+            it--;
+        }
+        else if(command=='R') {
+            it++;
+        }
+        else if(command=='D') {
+            breads.erase(it);
+        }
+        else if(command=='P') {
+            cin >> new_alphabet;
+            breads.insert(it, new_alphabet);
+        }
+    }
+
+    for(it=breads.begin(); it!=breads.end(); it++) {
+        cout << *it;
+    }
+
+    return 0;
+}
